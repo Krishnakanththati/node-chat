8fa3027bf2dbecd7a9c6ba94ab5a631b439610c6
Krishna Kanth <thati.krishnakanth@gmail.com>
2021-07-13 18:11:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const socketIO = require('socket.io');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// App setup
+const server = app.listen(PORT, () => {
+    console.log('App listening on Port -> http://localhost:' + PORT);
+});
+
+// Socket setup
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+    io.sockets.emit('auth', 'connection is success');
+    socket.on('chat', function (data) {
+        data['timestamp'] = new Date();
+        io.sockets.emit('chat', data);
+    });
+});
